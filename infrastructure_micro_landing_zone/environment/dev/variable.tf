46cd899d1c786cbde878d90f36c98cb866a5337f
anuragdevops2718 <anuragdev2718@gmail.com>
2025-11-24 18:30:09 +0530

    planning for dev infra with added featuress

--- infrastructure_micro_landing_zone/environment/dev/variable.tf
+++ infrastructure_micro_landing_zone/environment/dev/variable.tf
@@ -66,10 +66,10 @@ variable "kv_config" {
     name                        = string
     location                    = string
     resource_group_name         = string
-    enabled_for_disk_encryption = optional(boolean, true)
+    enabled_for_disk_encryption = optional(bool, true)
     tenant_id                   = string
     soft_delete_retention_days  = optional(number, 7)
-    purge_protection_enabled    = optional(boolean, false)
+    purge_protection_enabled    = optional(bool, false)
     sku_name                    = string
     access_policy = optional(map(object({
       tenant_id           = string
